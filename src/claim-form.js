f1746ca35a93b4941f8bc589d9dca91c4db1e73e
Jake Earing <jakeearing7@gmail.com>
2023-03-18 16:21:12 -0400

    Logo update, claim form update
    Logo and header have been changed, this is definitely the best iteration of the header/logo so far. Claim form and claim form style has been changed as well, just need to make the "description" label centered.

--- src/claim-form.js
+++ src/claim-form.js
@@ -71,129 +71,132 @@ export default function ClaimForm() {
     };
 
     return (
-        <form>
-            <div className="form-group">
-                <label htmlFor="first-name">First Name:</label>
-                <input type="text" id="first-name" />
-            </div>
+        <div class="form-container">
+            <form>
+                <div className="form-group">
+                    <label htmlFor="first-name">First Name:</label>
+                    <input type="text" id="first-name" />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="last-name">Last Name:</label>
-                <input type="text" id="last-name" />
-            </div>
+                <div className="form-group">
+                    <label htmlFor="last-name">Last Name:</label>
+                    <input type="text" id="last-name" />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="email">Email Address:</label>
-                <input type="email" id="email" />
-            </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email Address:</label>
+                    <input type="email" id="email" />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="phone">Phone Number:</label>
-                <input type="tel" id="phone" />
-            </div>
+                <div className="form-group">
+                    <label htmlFor="phone">Phone Number:</label>
+                    <input type="tel" id="phone" />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="zip-code">Zip Code:</label>
-                <input type="text" id="zip-code" value={zipCode} onChange={handleZipCodeChange} />
-            </div>
+                <div className="form-group">
+                    <label htmlFor="zip-code">Zip Code:</label>
+                    <input type="text" id="zip-code" value={zipCode} onChange={handleZipCodeChange} />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="claim-type">Type of Claim:</label>
-                <select id="claim-type" value={claimType} onChange={handleClaimTypeChange}>
-                    <option value="">Select Claim Type</option>
-                    <option value="home">Home</option>
-                    <option value="business">Business</option>
-                    <option value="auto">Auto</option>
-                    <option value="marine-aviation">Marine/Aviation</option>
-                </select>
-            </div>
+                <div className="form-group">
+                    <label htmlFor="claim-type">Type of Claim:</label>
+                    <select id="claim-type" value={claimType} onChange={handleClaimTypeChange}>
+                        <option value="">Select Claim Type</option>
+                        <option value="home">Home</option>
+                        <option value="business">Business</option>
+                        <option value="auto">Auto</option>
+                        <option value="marine-aviation">Marine/Aviation</option>
+                    </select>
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="damage-type">What kind of damage?</label>
-                <select id="damage-type" value={damageType} onChange={handleDamageTypeChange}>
-                    <option value="">Select damage type</option>
-                    <option value="hail">Hail damage</option>
-                    <option value="wind">Wind damage</option>
-                    <option value="water">Water damage</option>
-                    <option value="auto-accident">Auto accident</option>
-                    <option value="loss-of-income">Loss of income</option>
-                </select>
-            </div>
+                <div className="form-group">
+                    <label htmlFor="damage-type">What kind of damage?</label>
+                    <select id="damage-type" value={damageType} onChange={handleDamageTypeChange}>
+                        <option value="">Select damage type</option>
+                        <option value="hail">Hail damage</option>
+                        <option value="wind">Wind damage</option>
+                        <option value="water">Water damage</option>
+                        <option value="auto-accident">Auto accident</option>
+                        <option value="loss-of-income">Loss of income</option>
+                    </select>
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="insurance-company">Insurance Company:</label>
-                <input type="text" id="insurance-company" value={insuranceCompany} onChange={handleInsuranceCompanyChange} />
-            </div>
+                <div className="form-group">
+                    <label htmlFor="insurance-company">Insurance Company:</label>
+                    <input type="text" id="insurance-company" value={insuranceCompany} onChange={handleInsuranceCompanyChange} />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="date-of-loss">When did the loss occur?</label>
-                <input type="date" id="date-of-loss" value={dateOfLoss} onChange={handleDateOfLossChange} />
-            </div>
+                <div className="form-group">
+                    <label htmlFor="date-of-loss">When did the loss occur?</label>
+                    <input type="date" id="date-of-loss" value={dateOfLoss} onChange={handleDateOfLossChange} />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="already-filed-claim">Have you already filed a claim?</label>
-                <select id="already-filed-claim" value={alreadyFiledClaim} onChange={handleAlreadyFiledClaimChange}>
-                    <option value="no">No</option>
-                    <option value="yes">Yes</option>
-                </select>
-            </div>
+                <div className="form-group">
+                    <label htmlFor="already-filed-claim">Have you already filed a claim?</label>
+                    <select id="already-filed-claim" value={alreadyFiledClaim} onChange={handleAlreadyFiledClaimChange}>
+                        <option value="no">No</option>
+                        <option value="yes">Yes</option>
+                    </select>
+                </div>
 
-            {alreadyFiledClaim === 'yes' && (
-                <>
-                    <div className="form-group">
-                        <label htmlFor="claim-number">What is your claim number?</label>
-                        <input type="text" id="claim-number" value={claimNumber} onChange={handleClaimNumberChange} />
-                    </div>
+                {alreadyFiledClaim === 'yes' && (
+                    <>
+                        <div className="form-group">
+                            <label htmlFor="claim-number">What is your claim number?</label>
+                            <input type="text" id="claim-number" value={claimNumber} onChange={handleClaimNumberChange} />
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="adjuster-on-site">Has an adjuster been on site?</label>
-                        <select id="adjuster-on-site" value={adjusterOnSite} onChange={handleAdjusterOnSiteChange}>
-                            <option value="no">No</option>
-                            <option value="yes">Yes</option>
-                        </select>
-                    </div>
+                        <div className="form-group">
+                            <label htmlFor="adjuster-on-site">Has an adjuster been on site?</label>
+                            <select id="adjuster-on-site" value={adjusterOnSite} onChange={handleAdjusterOnSiteChange}>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="claim-denied">Has your claim been denied?</label>
-                        <select id="claim-denied" value={claimDenied} onChange={handleClaimDeniedChange}>
-                            <option value="no">No</option>
-                            <option value="yes">Yes</option>
-                        </select>
-                    </div>
+                        <div className="form-group">
+                            <label htmlFor="claim-denied">Has your claim been denied?</label>
+                            <select id="claim-denied" value={claimDenied} onChange={handleClaimDeniedChange}>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="loss-mitigation">Has loss mitigation begun?</label>
-                        <select id="loss-mitigation" value={lossMitigation} onChange={handleLossMitigationChange}>
-                            <option value="no">No</option>
-                            <option value="yes">Yes</option>
-                        </select>
-                    </div>
+                        <div className="form-group">
+                            <label htmlFor="loss-mitigation">Has loss mitigation begun?</label>
+                            <select id="loss-mitigation" value={lossMitigation} onChange={handleLossMitigationChange}>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="repairs-started">Have you started repairs?</label>
-                        <select id="repairs-started" value={repairsStarted} onChange={handleRepairsStartedChange}>
-                            <option value="no">No</option>
-                            <option value="yes">Yes</option>
-                        </select>
-                    </div>
+                        <div className="form-group">
+                            <label htmlFor="repairs-started">Have you started repairs?</label>
+                            <select id="repairs-started" value={repairsStarted} onChange={handleRepairsStartedChange}>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="contractor-selected">Have you selected a contractor?</label>
-                        <select id="contractor-selected" value={contractorSelected} onChange={handleContractorSelectedChange}>
-                            <option value="no">No</option>
-                            <option value="yes">Yes</option>
-                        </select>
-                    </div>
-                </>
-            )}
+                        <div className="form-group">
+                            <label htmlFor="contractor-selected">Have you selected a contractor?</label>
+                            <select id="contractor-selected" value={contractorSelected} onChange={handleContractorSelectedChange}>
+                                <option value="no">No</option>
+                                <option value="yes">Yes</option>
+                            </select>
+                        </div>
+                    </>
+                )}
 
-            <div className="form-group description">
-                <label htmlFor="description" class="above">Description of claim</label>
-                <textarea id="description" value={description} onChange={handleDescriptionChange} />
-            </div>
+                <div className="form-group description">
+                    <label htmlFor="description" class="above">Description of claim</label>
+                    <textarea id="description" value={description} onChange={handleDescriptionChange} />
+                </div>
+                <div className="submitButton">
+                <button type="submit">Submit</button>
+                </div>
 
-            <button type="submit">Submit</button>
-
-        </form>
+            </form>
+        </div>
     )
 }
