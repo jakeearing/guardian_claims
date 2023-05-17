77622215b91ec4b0510412e699f65dfe46a1e329
Jake Earing <jakeearing7@gmail.com>
2023-05-17 13:50:06 -0400

    Splash page component created
    Moved the Splash page to it's own component that has it's own css file for better organization and future reuse. Moved parts of the home style that will be used on multiple pages to index.js.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import Header from '../components/nav/Header.js';
 import Footer from '../components/nav/Footer.js';
+import Splash from '../components/Splash.js';
 import ClaimSteps from '../components/ClaimSteps.js';
 import ClaimTypes from '../components/ClaimTypes.js';
 import '../styles/style.css'
-import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <div class="container">
       <Header />
       <div className="splash-container">
-        <div className="splash-content">
-          <p>Are you struggling with your insurance claim?</p>
-          <p>With our simple intake process, getting started is easy. Just fill out the form,
-            and we will review your file to make sure you are a good fit for a Public Adjuster.
-            From start to finish, we keep you informed and updated every step of the way.</p>
-          <button><Link to="/claim">Get Guardian</Link></button>
-          <p>Let Guardian Claims be your protector and defender against unjust insurance practices. We're here to shield your claim and fight for your right to fair compensation.</p>
-        </div>
+        <Splash />
       </div>
       <div className="claimsteps-container">
         <ClaimSteps />
