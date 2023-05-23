f293bb353126747baa7fe0887f2082972ab70856
Jake Earing <jakeearing7@gmail.com>
2023-05-23 13:27:25 -0400

    "What We Do" component first version
    First version of the "What We Do" component, all of the text as well as the icon names are pulled from the content.json file. Also a small update was pushed that makes the 2nd "Square" of the secondary pages background color grey.

--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -5,7 +5,29 @@ import content from '../content.json';
 export default function WhatWeDo() {
   return (
     <div className="what-we-do-container">
-
+      <h2 className="title">{content.whatWeDo.heading}</h2>
+      <div className="icon-container">
+        <div className="icon-wrapper">
+        <img src={process.env.PUBLIC_URL + `/images/svg/what-we-do/${content.whatWeDo.iconOne}`} alt="Icon One"  className="icon" />
+          <p className="icon-text">{content.whatWeDo.iconOneText}</p>
+        </div>
+        <div className="icon-wrapper">
+        <img src={process.env.PUBLIC_URL + `/images/svg/what-we-do/${content.whatWeDo.iconTwo}`} alt="Icon Two"  className="icon" />
+          <p className="icon-text">{content.whatWeDo.iconTwoText}</p>
+        </div>
+        <div className="icon-wrapper">
+        <img src={process.env.PUBLIC_URL + `/images/svg/what-we-do/${content.whatWeDo.iconThree}`} alt="Icon Three"  className="icon" />
+          <p className="icon-text">{content.whatWeDo.iconThreeText}</p>
+        </div>
+        <div className="icon-wrapper">
+        <img src={process.env.PUBLIC_URL + `/images/svg/what-we-do/${content.whatWeDo.iconFour}`} alt="Icon Four"  className="icon" />
+          <p className="icon-text">{content.whatWeDo.iconFourText}</p>
+        </div>
+        <div className="icon-wrapper">
+        <img src={process.env.PUBLIC_URL + `/images/svg/what-we-do/${content.whatWeDo.iconFive}`} alt="Icon Five"  className="icon" />
+          <p className="icon-text">{content.whatWeDo.iconFiveText}</p>
+        </div>
+      </div>
     </div>
   );
 }
