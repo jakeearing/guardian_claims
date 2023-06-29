635ebad08dd1ceaa70cca5f67c42404c6f518cbd
jakeearing <jakeearing7@gmail.com>
2023-06-29 12:42:54 -0400

    Added image preloading on the home page to improve loading times
    After adding new individual pictures, the loading times were slowed down greatly. This led me to adding a useEffect hook to preload the images for Claim Steps and Claim Types when the components are loaded in.

--- src/components/ClaimSteps.js
+++ src/components/ClaimSteps.js
@@ -1,32 +1,45 @@
-import React from 'react';
-import '../styles/claim-steps.css'
-import ClaimForm from './ClaimForm.js';
+import React, { useEffect } from 'react';
+import '../styles/claim-steps.css';
 import { Link } from 'react-router-dom';
 import content from '../content.json';
 
 export default function ClaimSteps() {
+  useEffect(() => {
+    // Preload images
+    content.claimSteps.boxes.forEach(box => {
+      const img = new Image();
+      img.src = process.env.PUBLIC_URL + `/images/content/${box.image}`;
+    });
+  }, []);
 
-    return (
-        <div className="claim-steps-wrapper">
-            <h1>{content.claimSteps.headingThinOne} <span className="bold">{content.claimSteps.headingBold}</span> 
-            {content.claimSteps.headingThinTwo}</h1>
-            <h3><span className="thin">{content.claimSteps.description}</span></h3>
-            <div className="claim-steps-content">
-                {content.claimSteps.boxes.map((box, index) => (
-                    <div className="claim-step-box" key={index}>
-                        <img src={process.env.PUBLIC_URL + `/images/content/${box.image}`} alt="intakeForm" />
-                        <h2>{box.title}</h2>
-                        <p>{box.description}</p>
-                        {/* Check index 0,1,2 to display which button */}
-                        {index === 0 && (
-                            <Link to={box.buttonLink}>
-                                <button>{box.buttonText}</button>
-                            </Link>
-                        )}
-                    </div>
-                ))}
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="claim-steps-wrapper">
+      <h1>
+        {content.claimSteps.headingThinOne}{' '}
+        <span className="bold">{content.claimSteps.headingBold}</span>{' '}
+        {content.claimSteps.headingThinTwo}
+      </h1>
+      <h3>
+        <span className="thin">{content.claimSteps.description}</span>
+      </h3>
+      <div className="claim-steps-content">
+        {content.claimSteps.boxes.map((box, index) => (
+          <div className="claim-step-box" key={index}>
+            <img
+              src={process.env.PUBLIC_URL + `/images/content/${box.image}`}
+              alt="intakeForm"
+            />
+            <h2>{box.title}</h2>
+            <p>{box.description}</p>
+            {/* Check index 0,1,2 to display which button */}
+            {index === 0 && (
+              <Link to={box.buttonLink}>
+                <button>{box.buttonText}</button>
+              </Link>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/components/ClaimTypes.js
+++ src/components/ClaimTypes.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/claim-types.css';
 import content from '../content.json';
 
 export default function ClaimTypes() {
+  useEffect(() => {
+    // Preload images
+    content.claimTypes.claimCategories.forEach(category => {
+      const img = new Image();
+      img.src = process.env.PUBLIC_URL + `/images/content/${category.image}`;
+    });
+  }, []);
+
   return (
     <div className="claim-types-wrapper">
-      <h1><span className="thin">
-      {content.claimTypes.headingThin}</span>{content.claimTypes.headingBold}
+      <h1>
+        <span className="thin">{content.claimTypes.headingThin}</span>
+        {content.claimTypes.headingBold}
       </h1>
-      <h3><span className="thin">{content.claimTypes.description}</span></h3>
+      <h3>
+        <span className="thin">{content.claimTypes.description}</span>
+      </h3>
       <div className="claim-types-content">
         {content.claimTypes.claimCategories.map((category, index) => (
           <div className="claims-box" key={index}>
             <div className="image-wrapper">
-              <img src={process.env.PUBLIC_URL + `/images/content/${category.image}`} alt="intakeForm" />
+              <img
+                src={process.env.PUBLIC_URL + `/images/content/${category.image}`}
+                alt="intakeForm"
+              />
               <div className="overlay">
                 <h2>{category.title}</h2>
               </div>
