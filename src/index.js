56d0f14f53935854ac44b2c7fb91c83a1dfa9bf2
jakeearing <jakeearing7@gmail.com>
2023-07-10 14:53:53 -0400

    Google analytics functionality added
    Implemented google analytics into the website, using GuardianClaims Measurement ID.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './routes/App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from "react-ga4";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//Initialize GA4 for Google Analytics
+ReactGA.initialize("G-W902GQV7DE");
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-reportWebVitals();
+// Send Analytics Data
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
+
+reportWebVitals(SendAnalytics);
