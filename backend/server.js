94788fa089aa388c178543384ba308b050052c75
jakeearing <jakeearing7@gmail.com>
2023-08-08 14:52:10 -0400

    Moved backend into its own folder, separated dependencies for frontend and backend. Backend server port will always be 5000.
    As the Guardian Claims team prepares to have their website online, I separated the frontend and backend to be ready for deployment.

--- backend/server.js
+++ backend/server.js
@@ -76,7 +76,7 @@ app.post('/api/send-email', (req, res) => {
 });
 
 // Start the server
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
