fb1e024d1c8012d795700d7f9b206344e406e7a9
jakeearing <jakeearing7@gmail.com>
2023-12-22 20:30:20 -0500

    Update ClaimsContractors.js

--- src/pages/ClaimsContractors.js
+++ src/pages/ClaimsContractors.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ContractorForm from '../components/ContractorForm.js';
+import Footer from '../components/nav/Footer.js';
 import '../styles/claims.css';
 import content from '../content.json';
 
@@ -8,31 +9,40 @@ export default function ClaimsContractors() {
   return (
     <div>
       <div className="claims-form-wrapper">
-        <div className="logo-wrapper">
-          <Link to="/">
-            <img src={process.env.PUBLIC_URL + '/images/logos/logo-top.png'} alt="Guardian Claim Logo" />
-          </Link>
-        </div>
-        <div className="claims-content">
-          <div className="left-column">
-          <h3><span className="thin">{content.claimsContractors.leftColumnText}</span></h3>
-            <h2>{content.claimsContractors.leftColumnListTitle}</h2>
-            <ol>
-              {content.claimsContractors.leftColumnListItems.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ol>
+        <div className="claims-form-splash">
+          <div className="logo-wrapper">
+            <Link to="/">
+              <img src={process.env.PUBLIC_URL + '/images/logos/logo-top.png'} alt="Guardian Claim Logo" />
+            </Link>
           </div>
-          <div className="right-column">
-            <div className="claims-form">
-              <ContractorForm />
+          <div className="claims-splash-text">
+            <p>{content.claimsContractors.headerOne}</p>
+            <p>{content.claimsContractors.headerTwo}</p>
+          </div>
+        </div>
+        <div className="claims-content-background">
+          <div className="claims-content">
+            <div className="left-column">
+              <h2>{content.claimsContractors.leftColumnTitle}</h2>
+              <h3><span className="thin">{content.claimsContractors.leftColumnText}</span></h3>
+              <ul>
+                {content.claimsContractors.leftColumnListItems.map((item, index) => (
+                  <li key={index}>
+                    <h3>{item.itemTitle}</h3>
+                    <p>{item.itemText}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="right-column">
+              <div className="claims-form">
+                <ContractorForm />
+              </div>
             </div>
           </div>
         </div>
-        <h3 className="last-h3">
-          <span className="thin">{content.claimsContractors.lastSectionText}</span>
-        </h3>
       </div>
+      <Footer />
     </div>
   );
 }
