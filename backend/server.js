cfd8ced4b202ae537fbd0087d8f68bae0e652c13
jakeearing <jakeearing7@gmail.com>
2024-01-08 21:18:04 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,9 @@ app.post('/api/send-email', (req, res) => {
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
     }
   });
 
